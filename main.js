d96407bf5ef6ed5f91c2ab5a4849a19e504ac130
Ezel24 <134762823+Ezel24@users.noreply.github.com>
2023-08-06 11:45:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+name_of_the_student_array=[];
+function submit() 
+{
+    var name_1 = document.getElementById("name_of_the_student_1").value ;
+    var name_2 = document.getElementById("name_of_the_student_2").value ;
+    var name_3 = document.getElementById("name_of_the_student_3").value ;
+    var name_4 = document.getElementById("name_of_the_student_4").value ;
+
+    name_of_the_student_array.push(name_1);
+    name_of_the_student_array.push(name_2);
+    name_of_the_student_array.push(name_3);
+    name_of_the_student_array.push(name_4);
+
+    console.log(name_of_the_student_array);
+    document.getElementById("display_name").innerHTML=name_of_the_student_array
+}
